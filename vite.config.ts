d8538eef32595984b757827a4e49d4d90d384633
Peter van Hardenberg <pvh@pvh.ca>
2022-10-03 15:46:50 -0700

    worked to deploy to pvh

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: "blutack",
   plugins: [topLevelAwait(), react(), wasm()],
 
   optimizeDeps: {
