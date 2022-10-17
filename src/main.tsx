82d883493541a69b03de689f2ea5af97fbaba9fe
Peter van Hardenberg <pvh@pvh.ca>
2022-10-17 11:15:15 -0700

    fix case

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Root from "./components/Root"
-import "./app.css"
+import "./App.css"
 import "./ibm-plex.css"
 import "./vendor/line-awesome/css/line-awesome.min.css"
 
