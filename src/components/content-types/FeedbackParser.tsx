182e2b2a1b48a9b0fd8bd251da12430ee4c688bd
Geoffrey Litt <gklitt@gmail.com>
2022-10-25 22:04:37 -0400

    more work on feedback parser

--- src/components/content-types/FeedbackParser.tsx
+++ src/components/content-types/FeedbackParser.tsx
@@ -2,30 +2,44 @@ import React, { useCallback } from "react"
 
 import * as ContentTypes from "../pushpin-code/ContentTypes"
 import Content, { ContentProps, EditableContentProps } from "../Content"
-import { PushpinUrl } from "../pushpin-code/ShareLink"
+import {
+  createDocumentLink,
+  createWebLink,
+  parseDocumentLink,
+  PushpinUrl,
+} from "../pushpin-code/ShareLink"
 import ListItem from "../ui/ListItem"
 import Badge from "../ui/Badge"
 import ContentDragHandle from "../ui/ContentDragHandle"
 import TitleWithSubtitle from "../ui/TitleWithSubtitle"
 import "./FeedbackParser.css"
-import { useDocument } from "automerge-repo-react-hooks"
+import { useDocument, useRepo } from "automerge-repo-react-hooks"
 
-import { DocHandle } from "automerge-repo"
+import { DocHandle, DocumentId } from "automerge-repo"
 import * as ImportData from "../pushpin-code/ImportData"
 import CenteredStack from "../ui/CenteredStack"
 import Heading from "../ui/Heading"
+import { TextDoc } from "./TextContent"
 
-type FeedbackType = "positive" | "suggestion"
+type FeedbackType = "compliment" | "suggestion" | "question"
 
 interface FeedbackItem {
-  text: string
-  originalTextDocUrl: PushpinUrl
-  type: FeedbackType
+  summary: string
+  excerpt: string
+  _type: FeedbackType
 }
 
 interface Doc {
+  /* TODO: we use this "doc type" to signal that a bot should act on this doc;
+     but seems like we'd want something more like polymorphic cambria schemas? */
+  _type: "FeedbackParser"
   title?: string
-  textDocUrl?: PushpinUrl
+  textDocId?: DocumentId
+
+  /* HACK: a copy of the text from the linked doc.
+     remove once we figure out how to resolve linked docs in the server-side thing */
+  text?: string
+
   feedbackItems: FeedbackItem[]
 
   /* A signaling value used to ask an AI agent to parse the feedback
@@ -43,15 +57,25 @@ FeedbackParser.maxWidth = 24
 FeedbackParser.maxHeight = 36
 
 export default function FeedbackParser(props: ContentProps) {
+  const repo = useRepo()
   const [doc, changeDoc] = useDocument<Doc>(props.documentId)
+
+  const setTextDocId = (url: PushpinUrl) => {
+    const docId = parseDocumentLink(url)["documentId"]
+    const text = (repo.find(docId).doc as TextDoc).text.toString()
+    changeDoc((doc) => {
+      doc.textDocId = docId
+      doc.text = text
+      doc.feedbackItems = []
+      doc._requestParse = true
+    })
+  }
 
   const onDrop = useCallback((e: React.DragEvent) => {
     e.preventDefault()
 
     ImportData.importDataTransfer(e.dataTransfer, (url) => {
-      changeDoc((doc: Doc) => {
-        doc.textDocUrl = url
-      })
+      setTextDocId(url)
     })
   }, [])
 
@@ -60,7 +84,7 @@ export default function FeedbackParser(props: ContentProps) {
   }
 
   return (
-    <CenteredStack direction="column" centerText={false}>
+    <div className="FeedbackParser">
       <div className="FeedbackParser--tool-heading">
         <Heading>🪄 Magic Feedback Parser</Heading>
       </div>
@@ -70,27 +94,69 @@ export default function FeedbackParser(props: ContentProps) {
         onDragOver={preventDefault}
         onDragEnter={preventDefault}
       >
-        {doc.textDocUrl ? (
-          <Content context="workspace" url={doc.textDocUrl} />
+        {doc.textDocId ? (
+          <Content
+            context="workspace"
+            url={createDocumentLink("text", doc.textDocId)}
+          />
         ) : (
           <div>Drop a text document here</div>
         )}
       </div>
       <div className="FeedbackParser--suggestions">
-        <button onClick={() => changeDoc((d) => (d._requestParse = true))}>
-          Refresh suggestions
-        </button>
         {/* Show a message when the list is empty */}
         {doc.feedbackItems.length === 0 && (
           <div className="FeedbackParser--empty-list-message">
-            No feedback items yet
+            Processing...
           </div>
         )}
-        {doc.feedbackItems.map((item) => (
-          <div>{item.text}</div>
-        ))}
+
+        <Heading>👍 Compliments</Heading>
+
+        <div className="FeedbackParser--feedback-category">
+          {doc.feedbackItems
+            .filter((item) => item._type === "compliment")
+            .map((item) => (
+              <div className="FeedbackParser--feedback-item">
+                <div>
+                  <strong>{item.summary}</strong>
+                </div>{" "}
+                {item.excerpt}
+              </div>
+            ))}
+        </div>
+
+        <div className="FeedbackParser--feedback-category">
+          <Heading>💬 Suggestions</Heading>
+
+          {doc.feedbackItems
+            .filter((item) => item._type === "suggestion")
+            .map((item) => (
+              <div className="FeedbackParser--feedback-item">
+                <div>
+                  <strong>{item.summary}</strong>
+                </div>
+                {item.excerpt}
+              </div>
+            ))}
+        </div>
+
+        <div className="FeedbackParser--feedback-category">
+          <Heading>❓ Questions</Heading>
+
+          {doc.feedbackItems
+            .filter((item) => item._type === "question")
+            .map((item) => (
+              <div className="FeedbackParser--feedback-item">
+                <div>
+                  <strong>{item.summary}</strong>
+                </div>{" "}
+                {item.excerpt}
+              </div>
+            ))}
+        </div>
       </div>
-    </CenteredStack>
+    </div>
   )
 }
 
@@ -99,8 +165,10 @@ function preventDefault(e: React.SyntheticEvent) {
 }
 
 function create(unusedAttrs: any, handle: DocHandle<any>) {
-  handle.change((doc) => {
+  handle.change((doc: Doc) => {
+    doc._type = "FeedbackParser"
     doc.feedbackItems = []
+    doc._requestParse = false
   })
 }
 
