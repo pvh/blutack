35a30911658c104c21d8785ba13c2daa6d405c3b
Paul Sonnentag <paul.sonnentag@googlemail.com>
2022-12-16 16:10:36 +0100

    render RawView with react-json-view

--- src/components/content-types/RawView.tsx
+++ src/components/content-types/RawView.tsx
@@ -10,52 +10,19 @@ import TitleWithSubtitle from "../ui/TitleWithSubtitle"
 import { DocumentWithTitle } from "./workspace/Workspace"
 import { ContentProps, EditableContentProps } from "../Content"
 import "./RawView.css"
-import {
-  createDocumentLink,
-  createWebLink,
-  isPushpinUrl,
-} from "../pushpin-code/Url"
-import { DocumentId } from "../../../../automerge-repo"
+import ReactJson from "react-json-view"
 
 export default function RawView(props: ContentProps) {
   const [doc] = useDocument(props.documentId)
 
-  const jsonString = JSON.stringify(
-    doc,
-    (key, value) => {
-      if (typeof value !== "string") {
-        return value
-      }
-
-      if (isPushpinUrl(value)) {
-        return `<a href=${createWebLink(window.location, value)}>${value}</a>`
-      }
-
-      if (isDocumentId(value)) {
-        const href = createWebLink(
-          window.location,
-          createDocumentLink("raw", value as DocumentId)
-        )
-
-        return `<a href=${href}>${value}</a>`
-      }
-
-      return value
-    },
-    2
-  )
+  if (!doc) {
+    return null
+  }
 
   return (
-    <pre
-      className="RawView"
-      dangerouslySetInnerHTML={{ __html: jsonString }}
-    ></pre>
-  )
-}
-
-function isDocumentId(value: any) {
-  return /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/.test(
-    value
+    <div className="RawView">
+      <ReactJson src={JSON.parse(JSON.stringify(doc))} />
+    </div>
   )
 }
 
