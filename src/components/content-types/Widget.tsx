8378c6990a72aab87e8d76c133dcba351bfc6a12
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-06-17 19:04:18 +0100

    Add nicer editor and catch errors

--- src/components/content-types/Widget.tsx
+++ src/components/content-types/Widget.tsx
@@ -1,9 +1,14 @@
-import React, { ChangeEventHandler, useMemo } from "react"
+import React, { useEffect, useMemo, useRef } from "react"
 import { ContentType } from "../pushpin-code/ContentTypes"
 import { useDocument } from "automerge-repo-react-hooks"
 import { ContentProps } from "../Content"
 import { DocHandle } from "automerge-repo"
 import { html } from "htm/react"
+import { EditorView, keymap } from "@codemirror/view"
+import { basicSetup } from "codemirror"
+import { javascript } from "@codemirror/lang-javascript"
+import { indentWithTab } from "@codemirror/commands"
+import { ErrorBoundary } from "react-error-boundary"
 
 export interface WidgetDoc {
   title: string
@@ -12,6 +17,7 @@ export interface WidgetDoc {
 
 export default function Widget(props: ContentProps) {
   const [doc, changeDoc] = useDocument<WidgetDoc>(props.documentId)
+  const errorBoundaryRef = useRef<ErrorBoundary | null>(null)
 
   const source = doc?.source
 
@@ -20,31 +26,75 @@ export default function Widget(props: ContentProps) {
       return undefined
     }
 
-    return new Function("context", `with (context) { return ${doc.source} }`)({
-      html,
-    })
+    if (errorBoundaryRef.current) {
+      errorBoundaryRef.current.resetErrorBoundary()
+    }
+
+    try {
+      return new Function("context", `with (context) { return ${doc.source} }`)({
+        html,
+      })
+    } catch (err) {
+      return undefined
+    }
   }, [source])
 
   if (!doc) {
     return null
   }
 
-  const onChangeSource: ChangeEventHandler<HTMLTextAreaElement> = (evt) => {
+  const onChangeSource = (source: string) => {
     changeDoc((doc) => {
-      doc.source = evt.target.value
+      doc.source = source
     })
   }
 
   return (
-    <div>
-      <textarea value={doc.source} onChange={onChangeSource} />
-      <hr />
-      {View && <View doc={doc} changeDoc={changeDoc} />}
+    <div onDoubleClick={(evt) => evt.stopPropagation()}>
+      <CodeEditor source={doc.source} onChangeSource={onChangeSource} />
+      <ErrorBoundary fallback={<div>Something went wrong</div>} ref={errorBoundaryRef}>
+        {View && <View doc={doc} changeDoc={changeDoc} />}
+      </ErrorBoundary>
     </div>
   )
 }
 
-const EXAMPLE_SOURCE = `() => {
+interface CodeEditorProps {
+  source: string
+  onChangeSource: (source: string) => void
+}
+
+function CodeEditor({ source, onChangeSource }: CodeEditorProps) {
+  const editorRef = useRef(null)
+
+  useEffect(() => {
+    if (!editorRef.current) {
+      return
+    }
+
+    const view = new EditorView({
+      doc: source,
+      extensions: [basicSetup, javascript(), keymap.of([indentWithTab])],
+      dispatch(transaction) {
+        view.update([transaction])
+
+        if (transaction.docChanged) {
+          const newValue = view.state.doc.toString()
+          onChangeSource(newValue)
+        }
+      },
+      parent: editorRef.current,
+    })
+
+    return () => {
+      view.destroy()
+    }
+  }, [])
+
+  return <div ref={editorRef} onKeyDown={(evt) => evt.stopPropagation()} />
+}
+
+const EXAMPLE_SOURCE = `({doc, changeDoc}) => {
   return html\`
     <div>Hello world!</div>
   \`
